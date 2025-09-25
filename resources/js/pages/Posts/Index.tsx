f7f833bb1d1319b9622e7977f99cd70f34c3af17
Alex Balak <alex.balak@outlook.com>
2025-09-25 14:29:37 +0200

    working read from the database to the react view

--- resources/js/pages/Posts/Index.tsx
+++ resources/js/pages/Posts/Index.tsx
@@ -1,15 +1,40 @@
 import AppLayout from '@/layouts/app-layout';
 import { Head } from '@inertiajs/react';
 
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 
-export default function Index() {
+
+export default function Index({ posts }: { posts: any }) {
     return (
         <AppLayout>
             <Head title="Posts" />
            <div>
             <h1>Posts</h1>
+            <Table>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead>Title</TableHead>
+                        <TableHead>Body</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {posts.map((post: any) => (
+                        <TableRow key={post.id}>
+                            <TableCell>{post.title}</TableCell>
+                            <TableCell>{post.body}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
            </div>
         </AppLayout>
     );
-}
+}   
